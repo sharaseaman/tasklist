c5f0c7c56d7e56866cf896c40fd01e2c0a77cf45
sharaseaman <shara.seaman@gmail.com>
2017-12-20 17:40:02 -0600

    delete and filter tasks

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ loadEventListeners();
 function loadEventListeners() {
     //Add task event
     form.addEventListener('submit', addTask); 
+    // Remove task item by delete button
+    taskList.addEventListener('click', removeTask);
+    // Clear all items on clear button click
+    clearBtn.addEventListener('click', clearTasks);
+    //Filter task
+    filter.addEventListener('keyup', filterTasks);
+
 }
 
 //Add Task
@@ -43,4 +50,37 @@ function addTask(e){
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')){
+        e.target.parentElement.parentElement.remove();
+    }
+}
+
+// Clear All Tasks
+function clearTasks(e) {
+    // taskList.innerHTML = '';
+    // or - Faster Option
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    }
+
+}
+
+
+
+//Filter tasks
+function filterTasks(e){
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
 }
